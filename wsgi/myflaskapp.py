91cd1c4254d0e0e8c4bbe3c631d722c2a5df350e
Sérgio Berlotto <sergio.berlotto@gmail.com>
2013-11-21 11:16:32 -0200

    Versao inicial da app1

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,10 +1,12 @@
-from flask import Flask
+# -*- encoding: utf-8 -*-
+
+from flask import Flask, redirect, render_template, abort
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+	return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
-
+if __name__=="__main__":
+	app.run()

f799cbdcc6e15191cc07fe63b0a4044e0c55856d
Sérgio Berlotto <sergio.berlotto@gmail.com>
2013-12-05 15:06:06 -0200

    Config for while...

---
+++ wsgi/config.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+
+#API COnfiguration
+BASE_API_PREFIX = "htts://"
+BASE_API_DOMAIN = "broker.getupcloud.com"
+
+API_USER = "demo@getupcloud.com"
+API_PASSWD = "aFbe7g<jt]nM"
+
+#for the -c parameter of AB
+AB_TEST_CONCURRENCY = [40, 60, 80]
+#for the -n parameter 0f AB
+AB_TEST_REQUESTS = 150
+
+REDIS_SERVER = "localhost"
+REDIS_PASSWORD = "ZTNiMGM0NDI5OGZjMWMxNDlhZmJmNGM4OTk2ZmI5"
+
+#App2 Configuration
+
+#The namespace where the APP2 will be created
+NAMESPACE_APP2 = "demo"
+
